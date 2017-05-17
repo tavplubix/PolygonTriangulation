a3cf07ffc492b42f5912a2315a92f5463476dfea
SadDwarf <laidegor@gmail.com>
2017-05-18 02:17:40 +0300

    qwerty

--- main.cpp
+++ main.cpp
@@ -4,5 +4,15 @@ int main() {
     // Здесь должно быть чнение вешриш из файла
     // создание по ним класса Polygon
     // вызов метода и вывод триангуляции
+    int n;
+    std::cin >> n;
+    std::vector<std::pair<double>> a(n);
+    for (int i = 0; i < n; ++i) {
+        double x, y;
+        std::cin >> x >> y;
+        a[i].first = x;
+        a[i].second = y;
+    }
+    Polygon pol(a.begin(), a.end());
     return 0;
 }
